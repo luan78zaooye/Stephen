c08ae7716cbe646bb7f4b85301e9fcfde04f1fc2
luan78zaooye <stephen.stead.7459@gmail.com>
2024-01-17 09:27:26 -0800

    modify

--- Experian/Jenkins/RedshiftServerlessAutomation/create_redshift_serverless.py
+++ Experian/Jenkins/RedshiftServerlessAutomation/create_redshift_serverless.py
@@ -9,6 +9,8 @@ from pytz import timezone
 now = datetime.now(timezone('US/Pacific'))
 now_date = now.strftime("%Y%m%d")
 now_time = now.strftime("%Y%m%d_%H%M%S")
+namespaceName = f'ecs-{now_date}'
+workgroupName = "prod-rssls-01"
 
 
 # login
@@ -26,45 +28,45 @@ def set_boto_session(accountId=None, roleName=None):
     botosession = boto3.session.Session(**creds)
     return botosession
 
-# new password for admin user
+
 def getAdminPassword():
     pw_length = 16
     admin_pw = ""
-    
+
     for i in range(pw_length):
         num = random.randint(0, 9)
         letter = chr(random.randint(97, 122))
         admin_pw += str(random.choice([num, letter]))
-        
+
     # replace 1 or 2 characters with a number for the front half:
     for i in range(random.randrange(1, 3)):
         replace_index = random.randrange(len(admin_pw) // 2)
         admin_pw = admin_pw[:replace_index] + \
-               str(random.randrange(10)) + admin_pw[replace_index + 1:]
+                   str(random.randrange(10)) + admin_pw[replace_index + 1:]
 
     # replace 1 or 2 characters with a uppercase letter for the back half:
     for i in range(random.randrange(1, 3)):
         replace_index = random.randrange(len(admin_pw) // 2, len(admin_pw))
         admin_pw = admin_pw[:replace_index] + \
-            random.choice(string.ascii_uppercase) + admin_pw[replace_index + 1:]
-        
+                   random.choice(string.ascii_uppercase) + admin_pw[replace_index + 1:]
+
     return admin_pw
-        
+
+
 # save to Secret Manager
 def loadToSecretManager(adminUsername, adminUserPassword):
     secretsmanagerClient = boto3.client('secretsmanager', region_name="us-west-2")
     SecretString = '{"adminUser":"%s","Password":"%s"}' % (
-       adminUsername, adminUserPassword)
+        adminUsername, adminUserPassword)
     response = secretsmanagerClient.create_secret(Name=f'redshiftSecret-{now_time}',
                                                   SecretString=SecretString)
 
+
 # create nemespace and workgroup for serverless
 def createNamespaceWorkgroup(session, accountId, roleName):
-    adminUserPW = getAdminPassword()
     rsServerlessClient = session.client("redshift-serverless", region_name="us-west-2")
-    namespaceName = f'ecs-{now_date}'
-    workgroupName = "prod-rssls-01"
-
+    # generate password for admin user
+    adminUserPW = getAdminPassword()
     # create namespace
     namespaceResponse = rsServerlessClient.create_namespace(
         namespaceName=namespaceName,
@@ -75,7 +77,7 @@ def createNamespaceWorkgroup(session, accountId, roleName):
             f"arn:aws:iam::{accountId}:role/{roleName}"
         ],
         kmsKeyId='c3bc717c-287b-46b4-8721-e26b3c4f2431',
-        logExports=['useractivitylog','userlog','connectionlog'],
+        logExports=['useractivitylog', 'userlog', 'connectionlog'],
         tags=[
             {
                 'key': 'Type',
@@ -106,20 +108,19 @@ def createNamespaceWorkgroup(session, accountId, roleName):
                 'value': 'dbteam'
             }
         ]
-        
+
     )
 
     while True:
         response = rsServerlessClient.get_namespace(namespaceName=namespaceName)
         time.sleep(10)
-        if response['namespace']['status'] == "AVAILABLE":   
+        if response['namespace']['status'] == "AVAILABLE":
             break
 
-    
     # save admin user name and password to secret manager       
     adminUsername = response['namespace']['adminUsername']
     print("#" * 20, "loadToSecretManager", "#" * 20)
-    loadToSecretManager(adminUsername, adminUserPW) 
+    loadToSecretManager(adminUsername, adminUserPW)
 
     # create workgroup
     workgroupResponse = rsServerlessClient.create_workgroup(
@@ -171,12 +172,11 @@ def createNamespaceWorkgroup(session, accountId, roleName):
         time.sleep(10)
         if response['workgroup']['status'] == "AVAILABLE":
             break
-    
-    
+
 
 if __name__ == "__main__":
     print("#" * 20, "set_boto_session", "#" * 20)
     session = set_boto_session("251338191197", "redshift_serverless_automation")
     print("#" * 20, "createNamespaceWorkgroup", "#" * 20)
     createNamespaceWorkgroup(session, "251338191197", "redshift_serverless_automation")
-    
+
