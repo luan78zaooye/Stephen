30c41ad92423b00a0ac48d9c5c6376d4fdd7bca1
luan78zaooye <stephen.stead.7459@gmail.com>
2024-01-17 14:20:59 -0800

    commit

--- Experian/Jenkins/RedshiftServerlessAutomation/create_redshift_serverless_datashare.py
+++ Experian/Jenkins/RedshiftServerlessAutomation/create_redshift_serverless_datashare.py
@@ -3,62 +3,46 @@ import os
 import time
 from pytz import timezone
 from datetime import datetime
+from create_redshift_serverless import set_boto_session
+from create_redshift_serverless import namespaceName, workgroupName
 
 now = datetime.now(timezone('US/Pacific'))
 now_str = now.strftime("%Y%m%d")
 
 
-# key for test
-# os.environ['AWS_ACCESS_KEY_ID']="xxx"
-# os.environ['AWS_SECRET_ACCESS_KEY']="xxx"
-
-# login
-def set_boto_session(accountId=None, roleName=None):
-    creds = {}
-    if accountId and roleName:
-        role_arn = f"arn:aws:iam::{accountId}:role/{roleName}"
-        sts_client = boto3.client('sts')
-        stsResponse = sts_client.assume_role(RoleArn=role_arn, RoleSessionName=roleName)
-        creds['aws_access_key_id'] = stsResponse['Credentials']['AccessKeyId']
-        creds['aws_secret_access_key'] = stsResponse['Credentials']['SecretAccessKey']
-        creds['aws_session_token'] = stsResponse['Credentials']['SessionToken']
-    else:
-        print("invalid `accoundId` or `roleName`!")
-    botosession = boto3.Session(**creds)
-    return botosession, creds
-
-# get info
-def getNamespaceWorkgroup(session, namespaceName):
+def getNamespaceId(session, namespaceName):
     rsServerlessClient = session.client("redshift-serverless", region_name="us-west-2")
     namespaceResponse = rsServerlessClient.get_namespace(namespaceName=namespaceName)
-    namespaceID = namespaceResponse['namespace']['namespaceId']
+    namespaceId = namespaceResponse['namespace']['namespaceId']
 
 # create datashare from physical cluster
-def createDatashare(session, namespaceID):
+def createDatashare(session, namespaceId):
     redshiftDataClient = session.client("redshift-data", region_name="us-west-2")
-    cluster_identifier = 'redshift-cluster'
+    cluster_identifier = 'prod-rsraw-01'
     database = 'dev'
-    db_user = 'awsuser'
-
+    consumer_namespace = namespaceId
+    # sql scripts for producer
     share_name = f"rawToServerless-{now_str}"
-    sql_create_datashare = f"create datashare {share_name};"
-    sql_create_datashare += f"alter datashare {share_name} add all tables in schema event;"
-    sql_create_datashare += f"grant usage on datashare {share_name} to namespace '{namespaceID}';"
-
+    sql_create_datashare = f"CREATE DATASHARE {share_name};"
+    sql_create_datashare += f"GRANT USAGE ON DATASHARE {share_name} TO NAMESPACE '{consumer_namespace}';"
+    sql_create_datashare += f"ALTER DATASHARE {share_name} ADD SCHEMA event;"
+    sql_create_datashare += f"ALTER DATASHARE {share_name} ADD ALL TABLES IN SCHEMA event;"
+    sql_create_datashare += f"ALTER DATASHARE {share_name} SET INCLUDENEW = TRUE FOR SCHEMA event;"
+    # add more schema to datashare
+    ##########
+    ##########
     physicalResponse = redshiftDataClient.execute_statement(ClusterIdentifier=cluster_identifier,
                                                             Database=database,
-                                                            DbUser=db_user,
                                                             Sql=sql_create_datashare)
 
     # physicalResponseId = physicalResponse['Id']
 
-    sql_query_datashare = "show datashares;"
+    sql_query_datashare = "SHOW DATASHARES;"
     print("-" * 20, "Waiting for creating data share")
 
     while True:
         physicalResponse = redshiftDataClient.execute_statement(ClusterIdentifier=cluster_identifier,
                                                                 Database=database,
-                                                                DbUser=db_user,
                                                                 Sql=sql_query_datashare)
         physicalResponseId = physicalResponse['Id']
         time.sleep(10)
@@ -70,18 +54,17 @@ def createDatashare(session, namespaceID):
     """From data share create DB for Serverless"""
     index = shareNames.index(share_name)
     print("-" * 20, f"data share `{share_name}` created")
-    producer_account = list(response['Records'][index][-2].values())[0]
     producer_namespace = list(response['Records'][index][-1].values())[0]
     return producer_account, producer_namespace, share_name
 
 
 # create DB for serverless, from physical cluster data share
-def createDBforServerless(session, producer_account, producer_namespace, workgroupName, share_name):
+def createDBforServerless(session, producer_namespace, workgroupName, share_name):
     redshiftDataClient = session.client("redshift-data", region_name="us-west-2")
-    sql_createForServerless = f"create database share_db from datashare {share_name} of account '{producer_account}' namespace '{producer_namespace}';"
-
-    sql_createForServerless += "grant usage on database share_db to admin;"
-    sql_createForServerless += "grant usage on schema public to admin;"
+    # sql scripts for consumer
+    sql_createForServerless = f"CREATE DATABASE ecswarehouse FROM DATASHARE {share_name} OF NAMESPACE '{producer_namespace}';"
+    sql_createForServerless += "CREATE EXTERNAL SCHEMA event FROM REDSHIFT DATABASE ecswarehouse SCHEMA event;"
+  # sql_createForServerless += "GRANT USAGE ON SCHEMA event to group/user;"
 
     serverlessResponse = redshiftDataClient.execute_statement(Database="dev", WorkgroupName=workgroupName,
                                                               Sql=sql_createForServerless)
@@ -96,7 +79,7 @@ def createDBforServerless(session, producer_account, producer_namespace, workgro
 # query from serverless to test if the DB is created from data share successfully
 def testQuery(session, workgroupName):
     redshiftDataClient = session.client("redshift-data", region_name="us-west-2")
-    sql_test = "select * from share_db.public.tableshare limit 2;"
+    sql_test = "select * from event.xx limit 2;"
     queryFromServerless = redshiftDataClient.execute_statement(Database="dev", WorkgroupName=workgroupName,
                                                                Sql=sql_test)
     time.sleep(3)
@@ -108,13 +91,11 @@ def testQuery(session, workgroupName):
 if __name__ == "__main__":
     print("#" * 20, "set_boto_session", "#" * 20)
     session = set_boto_session("251338191197", "redshift_serverless_automation")
-    print("#" * 20, "createNamespaceWorkgroup", "#" * 20)
-    workgroupName = "prod-rssls-01"
-    namespaceID = getNamespaceID(session, "ecs-20240116")
+    print("#" * 20, "getNamespaceId", "#" * 20)
+    namespaceId = getNamespaceId(session, namespaceName)
     print("#" * 20, "createDatashare", "#" * 20)
-    producer_account, producer_namespace, share_name = createDatashare(session, namespaceID)
-    # time.sleep(60)
+    producer_account, producer_namespace, share_name = createDatashare(session, namespaceId)
     print("#" * 20, "createDBforServerless", "#" * 20)
-    createDBforServerless(session, producer_account, producer_namespace, workgroupName, share_name)
+    createDBforServerless(session, producer_namespace, workgroupName, share_name)
     print("#" * 20, "testQuery", "#" * 20)
     testQuery(session, workgroupName)
