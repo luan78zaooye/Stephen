56b49a5b37311fcc806ea083f8b90876a89ec0ae
luan78zaooye <92949480+luan78zaooye@users.noreply.github.com>
2024-01-18 09:12:06 -0800

    Update create_redshift_serverless_datashare.py

--- Experian/Jenkins/RedshiftServerlessAutomation/create_redshift_serverless_datashare.py
+++ Experian/Jenkins/RedshiftServerlessAutomation/create_redshift_serverless_datashare.py
@@ -81,7 +81,7 @@ def createDBforServerless(session,producer_namespace,workgroupName,share_name):
 # query from serverless to test if the DB is created from data share successfully
 def testQuery(session, workgroupName):
     redshiftDataClient = session.client("redshift-data",region_name="us-west-2")
-    sql_test = "select * from event.list limit 2;"
+    sql_test = "select * from dev.event.list limit 2;"
     queryFromServerless = redshiftDataClient.execute_statement(Database ="dev",WorkgroupName=workgroupName,Sql=sql_test)
     time.sleep(10)
     serverlessResponseId = queryFromServerless['Id']
