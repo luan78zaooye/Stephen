d027cf390cb4f002edded6b1119b8dda5b1a55cd
luan78zaooye <92949480+luan78zaooye@users.noreply.github.com>
2024-01-29 08:46:35 -0800

    Update redshift_serverless_costInfo_to_cluster.py

--- Experian/Jenkins/RedshiftServerlessAutomation/redshift_serverless_costInfo_to_cluster.py
+++ Experian/Jenkins/RedshiftServerlessAutomation/redshift_serverless_costInfo_to_cluster.py
@@ -17,7 +17,7 @@ before_str = before_date.strftime("%Y%m%d")
 def serverlessUnloadToS3(session,  serverlessWorkgroupName):
     redshiftDataClient = session.client("redshift-data", region_name="us-west-2")
 
-    unload_cost_query = f"UNLOAD($$SELECT TRUNC(CONVERT_TIMEZONE('US/Pacific', start_time)) as day,
+    unload_cost_query = f"UNLOAD($$SELECT TRUNC(CONVERT_TIMEZONE('US/Pacific', start_time)) as day, \
                                   (SUM(charged_seconds)/3600::double precision)*0.36 AS cost_incurred \
                                FROM sys_serverless_usage \
                                GROUP BY 1 \
